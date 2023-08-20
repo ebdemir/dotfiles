7586e1a00bacfdf41e709b9cdc153e06e72a7e4b
Emirhan Demir <e.demir@tkhsecurity.com>
2023-08-20 14:14:52 +0200

    Trying multiple monitor support for lock screen

--- i3/lock.sh
+++ i3/lock.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/bash 
-xterm -fullscreen -bg black -e asciiquarium & xtrlock; xdotool key --clearmodifiers q
+
+if [ $(command -v xrandr) ]; then
+    OFFSETS=$(\xrandr | \grep -w connected | \awk '{split($0, line, " "); if (line[3] == "primary") { split(line[4], offsets, "+"); print "+"offsets[2]"+"offsets[3] } else { split(line[3], offsets, "+"); print "+"offsets[2]"+"offsets[3] }}')
+    for o in $OFFSETS; do
+        xterm -geometry 0x0$o -fullscreen -bg black -class "lockscreen" -e asciiquarium &
+    done
+else
+    xterm -fullscreen -bg black -class "lockscreen" -e asciiquarium &
+fi
+
+xtrlock; xdotool key q
 
